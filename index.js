6743166092354e272edad604b9d360c474e21b6f
asad755 <143116178+asad755@users.noreply.github.com>
2024-01-07 04:36:40 +0500

    stady

---
+++ index.js
@@ -0,0 +1,93 @@
+#!user/bin/env node
+import inquirer from "inquirer";
+import { faker } from '@faker-js/faker';
+import chalk from "chalk";
+// (Requirement)
+// 1:User Data (Done)
+// 2:ATM Machine (Done)
+// 3:ATM Machine Functions
+console.log("When you enter a PIN code please type 1001 to 1009");
+;
+const createUser = () => {
+    const users = [];
+    for (let i = 0; i < 10; i++) {
+        const user = {
+            id: i,
+            pin: 1000 + i,
+            accountNo: Math.floor(1000000 * Math.random() * 200000),
+            balance: 1000000 * i,
+            name: faker.person.fullName()
+        };
+        users.push(user);
+    }
+    ;
+    return users;
+};
+//   2: Now we will create ATM Machine;
+const atmMachine = async (users) => {
+    const response = await inquirer.prompt({
+        type: "number",
+        message: "Please Enter Your PIN Code....",
+        name: "pin"
+    });
+    const user = users.find(val => val.pin == response.pin);
+    if (user) {
+        console.log(chalk.bgBlue(`Welcome to ${user.name}`));
+        atmFunc(user);
+        return;
+    }
+    else {
+        console.log(chalk.bgGreen("Invalid User"));
+    }
+};
+// 3:ATM Functions
+const atmFunc = async (user) => {
+    const ans = await inquirer.prompt({
+        type: "list",
+        name: "select",
+        message: "Select Option....",
+        choices: ["Withdraw", "Balance Inquiry", "Deposit", "Exit"]
+    });
+    if (ans.select == "Withdraw") {
+        const amount = await inquirer.prompt({
+            type: "number",
+            message: "Please Enter Your Amount",
+            name: "Rupees"
+        });
+        if (amount.Rupees > user.balance) {
+            return console.log(chalk.bgGreenBright("Sorry Your Amount Balance is Insufficient"));
+        }
+        if (amount.Rupees > 25000) {
+            return console.log("Sorry Your Transection limit is 25000");
+        }
+        console.log(`Withdraw amount ${amount.Rupees}`);
+        console.log(`Balance ${user.balance - amount.Rupees}`);
+    }
+    ;
+    if (ans.select == "Balance Inquiry") {
+        console.log(`Balance ${user.balance}`);
+        return;
+    }
+    ;
+    if (ans.select == "Deposit") {
+        const Deposit = await inquirer.prompt({
+            type: "number",
+            name: "rupee",
+            message: "Please Deposit your Amount"
+        });
+        console.log(`Deposit amount ${Deposit.rupee}`);
+        console.log(`New Balance is ${Deposit.rupee + user.balance}`);
+    }
+    if (ans.select == "Exit") {
+        console.log("Thanks for using ATM");
+    }
+    // console.log(ans);
+};
+let date = new Date();
+let convert = date.toDateString();
+// let copy = covnert.slice(0,15);
+console.log(convert);
+// console.log(response);
+const User = createUser();
+atmMachine(User);
+// console.log(User);
